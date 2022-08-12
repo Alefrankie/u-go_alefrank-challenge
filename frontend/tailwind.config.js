02a8dac4a94e2075dea4b7caf33d9cf6832b5093
Alefrankie <alefrank.m@gmail.com>
2022-08-12 15:27:38 -0400

    feat(frontend): colors in tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,12 @@
+const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: { ...colors, 'bright-turquoise': '#00E7F0', 'vivid-cerulean': '#009AF3' }
+    }
   },
   plugins: []
 }
