4610f10c2dba903d8caec15cb5a44bcb6e3273f7
Alefrankie <alefrank.m@gmail.com>
2022-08-12 21:20:03 -0400

    feat(frontend): hook to admin request

---
+++ frontend/src/lib/hooks/useFetch.ts
@@ -0,0 +1,16 @@
+async function post<T>(url: string, { body }: { body: T }): Promise<T> {
+  const res = await fetch(url, {
+    body: JSON.stringify(body)
+  })
+
+  const message = await res.text()
+  if (res.status >= 400) {
+    throw new Error(message)
+  }
+
+  const data: T = await res.json()
+
+  return data
+}
+
+export const useFetch = { post }
