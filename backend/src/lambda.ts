e8dbfe18ccf5cd9a9ea82b9e783d73d240b7856a
Alefrankie <alefrank.m@gmail.com>
2022-08-18 15:09:34 -0400

    triyin aws

--- backend/src/lambda.ts
+++ backend/src/lambda.ts
@@ -1,34 +1,25 @@
-import { Context, Handler } from 'aws-lambda'
-import { createServer, proxy } from 'aws-serverless-express'
-import { eventContext } from 'aws-serverless-express/middleware'
-import { Server } from 'http'
-
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-import { ExpressAdapter } from '@nestjs/platform-express'
+import serverlessExpress from '@vendia/serverless-express'
+import { Callback, Context, Handler } from 'aws-lambda'
 import { AppModule } from './_app/app.module'
 
-import express from 'express'
+let server: Handler
 
-// NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
-// due to a compressed response (e.g. gzip) which has not been handled correctly
-// by aws-serverless-express and/or API Gateway. Add the necessary MIME types to
-// binaryMimeTypes below
-const binaryMimeTypes: string[] = []
+// serverless deploy    Deploy changes
+// serverless info      View deployed endpoints and resources
+// serverless invoke    Invoke deployed functions
+// serverless --help    Discover more commands
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ValidationPipe())
+  await app.init()
 
-let cachedServer: Server
-
-async function bootstrapServer(): Promise<Server> {
-  if (!cachedServer) {
-    const expressApp = express()
-    const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp))
-    nestApp.use(eventContext())
-    await nestApp.init()
-    cachedServer = createServer(expressApp, undefined, binaryMimeTypes)
-  }
-  return cachedServer
+  const expressApp = app.getHttpAdapter().getInstance()
+  return serverlessExpress({ app: expressApp })
 }
 
-export const handler: Handler = async (event: any, context: Context) => {
-  cachedServer = await bootstrapServer()
-  return proxy(cachedServer, event, context, 'PROMISE').promise
+export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
+  server = server ?? (await bootstrap())
+  return server(event, context, callback)
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,14 +2,9 @@ import { Logger, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './_app/app.module'
 
-import serverlessExpress from '@vendia/serverless-express'
-import { Callback, Context, Handler } from 'aws-lambda'
-
 const PORT = 3001
 
-let server: Handler
-
-async function bootstrap(): Promise<Handler> {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe())
@@ -23,12 +18,6 @@ async function bootstrap(): Promise<Handler> {
   await app.init()
   await app.listen(process.env.PORT || PORT)
   Logger.log(`Application is running on: ${process.env.PORT || PORT}`)
-
-  const expressApp = app.getHttpAdapter().getInstance()
-  return serverlessExpress({ app: expressApp })
 }
 
-export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
-  server = server ?? (await bootstrap())
-  return server(event, context, callback)
-}
+bootstrap()
